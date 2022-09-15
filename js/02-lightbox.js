381aad8869c35fe09476457f2aa8b01f55d898b6
ZEugenii <zinich911@gmail.com>
2022-09-15 08:33:12 +0300

    add solved task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBoxRef = document.querySelector(".gallery");
+galleryBoxRef.addEventListener("click", onImgShow);
+
+function makeGalleryMarckup({ preview, original, description }) {
+  return `
+     <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="" title="${description}" />
+    </a> `;
+}
+
+const addPictureElements = galleryItems.map(makeGalleryMarckup).join("");
+
+galleryBoxRef.innerHTML = addPictureElements;
+
+function onImgShow(e) {
+  const target = e.target;
+  e.preventDefault();
+
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+  });
+}
